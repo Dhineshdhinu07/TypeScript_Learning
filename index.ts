fd967edb828e7ff310a76a7580d7eeb9ae203dac
Dhineshdhinu07 <dhinesh1278@gmail.com>
2024-10-16 01:38:50 +0530

    Upto Any type

--- index.ts
+++ index.ts
@@ -20,5 +20,24 @@
 // //Booleans
 // let isStudent: boolean = true;
 // console.log(isStudent);
+
 //---------------------------------------------//
+
 //Type Inference
+// let tech = "TypeScript"; //Here the compiler is inferring the type automatically. We didn't gave any type explicitly.
+// // tech = 12; //It will give us an error because its already declared as string.
+// let num = 8;
+// console.log(typeof num);
+// console.log(typeof tech);
+// console.log(tech);
+
+//------------------------------------------------//
+
+//Any Type
+
+// let color: any = "white";
+// color = 12;
+// color = true;
+// console.log(color); // The output will be "true"
+
+//-------------------------------------------------//
