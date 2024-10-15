008dc42a52a54b07ea6815116b6052a5fe7448cc
Dhineshdhinu07 <dhinesh1278@gmail.com>
2024-10-16 01:20:35 +0530

    index.ts page

---
+++ index.ts
@@ -0,0 +1,24 @@
+//Sample code:
+// type Person = {
+//     name: string;
+// };
+
+// const person: Person = {
+//     name: "John",
+// };
+// console.log(person.name);
+// ----------------------------------------//
+//Annotations
+//Strings
+// let myName: string = "Dhinesh M";
+// console.log(myName);
+
+// //Numbers
+// let myAge: number = 21;
+// console.log(myAge);
+
+// //Booleans
+// let isStudent: boolean = true;
+// console.log(isStudent);
+//---------------------------------------------//
+//Type Inference
